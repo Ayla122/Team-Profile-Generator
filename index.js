bae7269af1d8382aef65e6b574aa73af77e8926c
Ayla122 <ayladd122@gmail.com>
2021-10-23 19:37:22 -0700

    added node modules & team array

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
 //link to html page
-const generateHTML = require('./src/generateHTML')
+const generateHTML = require('./src/generateHTML');
+
+//team profiles 
+const Manager = require ('./lib/Manager');
+const Engineers = require ('./lib/Engineers');
+const Interns = require ('./lib/interns');
 
